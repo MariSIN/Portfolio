2fecd4e812a0c3c87a14b2432bb9a746b1b8e9c0
Mariana Nascimento <mariana.sn.silva@gmail.com>
2023-04-15 19:21:31 -0300

    deploy

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,9 +3,9 @@ import 'antd/dist/antd';
 import ptBR from 'antd/lib/locale/pt_BR';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
+import App from './App';
 import './index.css';
-import reportWebVitals from './src/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
